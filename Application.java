153fd90037f4070277cf8c524bb519c53f2e4f05
Matei Pribagu <mateipribagu@yahoo.com>
2018-04-27 16:57:35 +0300

    Another

--- Application.java
+++ Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Today is java training");
+		System.out.println("Today is java training and I modified this");
 	}
 
 }
